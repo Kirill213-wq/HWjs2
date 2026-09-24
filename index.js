eb1caf2540ca6e8efabc67c46447713797b05d04
Kirill213-wq <varizhuk2018@gmail.com>
2026-09-24 12:31:08 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-//dfvdfvsfs
+// 1.  додаємо це, щоб сервер розумів json з баді
+app.use(express.json());
+
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
-
 
 app.get('/stats', (req, res) => {
   res.json({
@@ -23,19 +24,40 @@ const products = [
   { id: 5, name: 'Dining Table', price: 450, category: 'furniture' },
 ];
 
-// GET /products — з підтримкою query-параметрів category та take
+// 2. Функція додавання продукту через проміз
+function addProduct(newProduct, isFail) {
+  return new Promise((resolve, reject) => {
+    
+    if (isFail) {
+      reject('Помилка збереження');
+    } else {
+      
+      const createdProduct = {
+        id: products.length + 1, 
+        name: newProduct.name,
+        price: newProduct.price,
+        category: newProduct.category,
+        image: newProduct.image || '' 
+      };
+
+      // Додаємо в масив
+      products.push(createdProduct);
+
+      
+      resolve(createdProduct);
+    }
+  });
+}
+
+// GET /products
 app.get('/products', (req, res) => {
   const { category, take } = req.query;
-
-  // Вихідний масив не змінюємо
   let result = products;
 
-  // Фільтрація 
   if (category) {
     result = result.filter((p) => p.category.toLowerCase() === category.toLowerCase());
   }
 
-  // Обмеження кількості через take 
   if (take !== undefined) {
     const limit = parseInt(take, 10);
     if (!isNaN(limit) && limit >= 0) {
@@ -46,11 +68,10 @@ app.get('/products', (req, res) => {
   res.status(200).json(result);
 });
 
-// GET /products/:id  отримання одного продукту за id
+// GET /products/:id
 app.get('/products/:id', (req, res) => {
   const productId = parseInt(req.params.id, 10);
 
-  // Валідація
   if (isNaN(productId)) {
     return res.status(400).json({ error: 'Invalid product ID' });
   }
@@ -65,7 +86,34 @@ app.get('/products/:id', (req, res) => {
 });
 
 
+app.post('/products', async (req, res) => {
+  const { name, price, category, image } = req.body;
+  
+ 
+  const isFail = req.query.fail === 'true';
+
+  // 1ша валідація перевірки 
+  if (!name || typeof price !== 'number' || price <= 0 || !category) {
+    return res.status(422).json({ error: 'Invalid product data' });
+  }
+
+  // 2га валідація перевірки на дублікат назви
+  const duplicate = products.find(p => p.name.toLowerCase() === name.toLowerCase());
+  if (duplicate) {
+    return res.status(409).json({ error: 'Conflict' });
+  }
+
+  // наш проміз
+  try {
+    const newProduct = await addProduct({ name, price, category, image }, isFail);
+    return res.status(201).json(newProduct); 
+  } catch (error) {
+    return res.status(500).json({ error: 'Server Error' }); 
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
